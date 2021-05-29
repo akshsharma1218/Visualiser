3780c5578947df8ef0b08afc5b70766caaa494d4
akshsharma <akshsharma1218@gmail.com>
2021-05-29 23:45:49 +0530

    started

---
+++ index.js
@@ -0,0 +1,7 @@
+const aksh_menu_element = document.querySelector(".aksh-menu");
+
+const div = document.querySelector(".ak_container");
+
+aksh_menu_element.addEventListener("click", function() {
+    div.classList.toggle("shekhar");
+})
